1af7e6117ee52c9df58866c516afea3e59d7bc63
spicyGrape <li.private0@gmail.com>
2024-04-08 18:37:16 +0100

    added calcNetForceXY

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,4 +1,5 @@
 public class Planet{
+	static final double G = 6.67e-11;
 	public double xxPos, yyPos, xxVel, yyVel, mass;
 	public String imgFileName;
 	public Planet(double xP, double yP, double xV, double yV, double m, String img){
@@ -19,6 +20,33 @@ public class Planet{
 	}
 	public double calcDistance(Planet p){
 		return Math.sqrt((yyPos-p.yyPos)*(yyPos-p.yyPos)+(xxPos-p.xxPos)*(xxPos-p.xxPos));
+	}
+	public double calcForceExertedBy(Planet p){
+		return G * mass * p.mass/(calcDistance(p) * calcDistance(p));
+	}
+	public double calcForceExertedByX(Planet p){
+		return calcForceExertedBy(p)*(p.xxPos-xxPos)/calcDistance(p);
+	}
+	public double calcForceExertedByY(Planet p){
+		return calcForceExertedBy(p)*(p.yyPos-yyPos)/calcDistance(p);
+	}
+	public double calcNetForceExertedByX(Planet[] allPlanets){
+		double netForceExertedByX = 0;
+		for ( Planet p : allPlanets ) {
+			if (!this.equals(p)){
+				netForceExertedByX = netForceExertedByX + calcForceExertedByX(p);
+			}
+		}
+		return netForceExertedByX;
+	}
+	public double calcNetForceExertedByY(Planet[] allPlanets){
+		double netForceExertedByY = 0;
+		for ( Planet p : allPlanets ) {
+			if (!this.equals(p)){
+				netForceExertedByY = netForceExertedByY + calcForceExertedByY(p);
+			}
+		}
+		return netForceExertedByY;
 	}
 }
 
