6c799f9f74f547a34cde92db8fa98e5e2a09de76
spicyGrape <li.private0@gmail.com>
2024-04-08 16:36:33 +0100

    Created Plant.java and added constructors

---
+++ proj0/Planet.java
@@ -0,0 +1,21 @@
+public class Planet{
+	public double xxPos, yyPos, xxVel, yyVel, mass;
+	public String imgFileName;
+	public Planet(double xP, double yP, double xV, double yV, double m, String img){
+		xxPos = xP;
+		yyPos = yP;
+		xxVel = xV;
+		yyVel = yV;
+		mass = m;
+		imgFileName = img;
+	}
+	public Planet(Planet p){
+		xxPos = p.xxPos;
+		yyPos = p.yyPos;
+		xxVel = p.xxVel;
+		yyVel = p.yyVel;
+		mass = p.mass;
+		imgFileName = p.imgFileName;
+	}
+
+}
