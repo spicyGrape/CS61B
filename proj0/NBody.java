4b1d235bee47cc9d89f27d67ca308c9acc8b1588
spicyGrape <li.private0@gmail.com>
2024-04-10 15:21:16 +0100

    added readPlanets

---
+++ proj0/NBody.java
@@ -0,0 +1,17 @@
+public class NBody{
+	public static double readRadius(String fileName){
+		In in = new In(fileName);
+		in.readInt();
+		return in.readDouble();
+	}
+	public Planet[] readPlanets(String fN){
+		In in = new In(fileName);
+		int numP = in.readInt();
+		planets = new Planet[numP];
+		in.readDouble();
+		for(int i = 0; i < numP; i=i+1){
+			planets[i] = Planet(in.readDouble(),in.readDouble(),in.readDouble(),in.readDouble(),in.readDouble(),in.readString());
+		}
+		return planets;
+	}
+}
