574499d46d1f42ca764ba97d6ffff6ee48574713
spicyGrape <li.private0@gmail.com>
2024-04-11 14:48:58 +0100

    added readPlanets and fixed bugs

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -4,13 +4,13 @@ public class NBody{
 		in.readInt();
 		return in.readDouble();
 	}
-	public Planet[] readPlanets(String fN){
-		In in = new In(fileName);
+	public static Planet[] readPlanets(String fN){
+		In in = new In(fN);
 		int numP = in.readInt();
-		planets = new Planet[numP];
+		Planet[] planets = new Planet[numP];
 		in.readDouble();
 		for(int i = 0; i < numP; i=i+1){
-			planets[i] = Planet(in.readDouble(),in.readDouble(),in.readDouble(),in.readDouble(),in.readDouble(),in.readString());
+			planets[i] = new Planet(in.readDouble(),in.readDouble(),in.readDouble(),in.readDouble(),in.readDouble(),in.readString());
 		}
 		return planets;
 	}
