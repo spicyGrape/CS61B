2ee74897727a2de8ab4b7163296280de07a518b2
spicyGrape <li.private0@gmail.com>
2024-04-11 15:06:24 +0100

    added main in NBody

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -14,4 +14,12 @@ public class NBody{
 		}
 		return planets;
 	}
+	public static void main(String[] args){
+		double T = Double.parseDouble(args[0]);
+		double dt = Double.parseDouble(args[1]);
+		String filename = args[2];
+		double uniRad = readRadius(filename);
+		Planet[] planets = readPlanets(filename);
+
+	}
 }
